1dd76582b35bd024107ee0f1f10ab2db8e3aa618
Ahmed Mostafa <Ahmed Mostafa>
2016-05-31 01:14:17 -0400

    adding the theme

---
+++ Empty/app.js
@@ -0,0 +1,31 @@
+angular.module('app', ['ngRoute', 
+  'ui.mask',
+  'ngMessages',
+  'mp.datePicker',
+  'ngMaterial'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      //.when('/', {
+      //  templateUrl: 'home/home.html',
+      //  controller: 'HomeController',
+      //  controllerAs: 'home'
+      //})
+      .when('/messages', {
+        templateUrl: 'messages/messages.html',
+        controller: 'messagesController',
+        controllerAs: 'messages'
+      })
+      .when('/appointments', {
+        templateUrl: 'appointments/appointments.html',
+        controller: 'appointmentsController',
+        controllerAs: 'appointments'
+      })
+      .when('/appointment', {
+        templateUrl: 'appointment/appointment.html',
+        controller: 'appointmentController',
+        controllerAs: 'appointment'
+      })
+      .otherwise({
+          redirectTo: '/appointments'
+      });
+  });
